a26c534426967832137b1a6e8f0aa6fafa3edaaa
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-01-16 06:28:53 +0700

    Revert back to default after finished collecting.

--- datacollector_chrome.py
+++ datacollector_chrome.py
@@ -17,12 +17,11 @@ def login(inp_uid):
     passwd.send_keys("cc") # [CONFIG] Anything will work...
     login_btn = driver.find_element(By.ID, "btnDangNhap")
     login_btn.send_keys(Keys.ENTER)
-def table_sel(num, semester): # (1..10, 1 or 2) | 1st is current year, 2nd is the previous,... ; Semester 1 or 2
+def table_sel(num, semester):
     driver.find_element(By.XPATH, f"//select[@id='ctl05_drpNamHoc']/option[{str(num)}]").click()
     driver.find_element(By.XPATH, f"//select[@id='ctl05_drpHocKy']/option[{str(semester)}]").click()
 def clean_attrib(html_str):
-    # Ignore common attrib maybe ? https://stackoverflow.com/questions/7470333/remove-certain-attributes-from-html-tags
-    cleaner = clean.Cleaner(safe_attrs_only=True, safe_attrs=frozenset({'colspan'}))
+    cleaner = clean.Cleaner(safe_attrs_only=True, safe_attrs=frozenset({'colspan'})) # Keep 'colspan' safe.
     return cleaner.clean_html(html_str)
 def logout(): driver.find_element(By.ID, "LinkButton2").click()
 colorama.init()
@@ -35,10 +34,10 @@ chrome_options.add_argument("--disable-in-process-stack-traces")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://qlttgddt.thuathienhue.edu.vn/home/dangnhap.aspx")
 
-for user_id in range(3000_387_654,3000_400_000): # [CONFIG] UID range
+for user_id in range(3000_350_000, 3000_400_000): # [CONFIG] UID range
     # 1. Login and fetch table.
     login(user_id)
-    table_sel(2,1) # [CONFIG] Year ?, Semester ?/2
+    table_sel(1,1) # [CONFIG] Year ?, Semester 1 or 2
     table = driver.find_element(By.XPATH, "//span[@id='ctl05_lblDanhSach']/table")
     table_raw = clean_attrib(minify_html.minify(table.get_attribute('outerHTML'), keep_closing_tags=True))
     table = BeautifulSoup(table_raw, "lxml")
@@ -46,7 +45,7 @@ for user_id in range(3000_387_654,3000_400_000): # [CONFIG] UID range
     # 2. Extract table header and correct it.
     table_head = table.tr.extract()
     table_head.name = "thead"
-    for elem in table_head.find_all("td"): elem.name = "th" # Replace all td with th
+    for elem in table_head.find_all("td"): elem.name = "th"
 
     # 3. Extract table body and re-append both back to table.
     table_body = table.tbody.extract()
@@ -62,7 +61,7 @@ for user_id in range(3000_387_654,3000_400_000): # [CONFIG] UID range
     if (str_cond == "Chưa cập nhật môn học") or (str_cond == "Học sinh chưa được phép xem kết quả học tập Học kỳ 1") or (str_cond == "Học sinh chưa được phép xem kết quả học tập Học kỳ 2"):
         print(f"{Fore.LIGHTRED_EX}Skipped UID {user_id} with the name {name}.{Fore.WHITE}")
     else:
-        with open(f"./collected/{str(user_id)}_{name}.json", "w", encoding="utf-8") as file:
+        with open(f"./collected/{user_id}_{name}.json", "w", encoding="utf-8") as file:
             df = pd.read_html(str(table))
             df_json = df[0].to_json(orient='records', force_ascii=False, indent=4)
             file.write(df_json)

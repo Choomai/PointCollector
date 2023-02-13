083135dc35504ee076984bf61090e8d860ad9b34
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-02-13 18:33:49 +0700

    How could i be this dumb

--- check_uids.py
+++ check_uids.py
@@ -8,8 +8,8 @@ ids = re.findall(r"[0-9]{10}",file_name.read())
 ids = [int(elem) for elem in ids]
 missing = []
 for i in range(config["start_UIDs"], config["end_UIDs"]):
-    if not i in ids: missing.append(i)
+    if not config["start_UIDs"] <= i < config["end_UIDs"]: missing.append(i)
 file_name_miss = open("./collected/missing.txt", "w", encoding="utf-8")
-file_name_miss.write(missing)
+file_name_miss.write(str(missing))
 file_name.close()
 file_name_miss.close()

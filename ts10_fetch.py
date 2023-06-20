c9822fb315f7d04d8a68bec12501cce055a76572
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-06-20 17:51:31 +0700

    Infinite retries

--- ts10_fetch.py
+++ ts10_fetch.py
@@ -7,6 +7,7 @@ from minify_html import minify
 import json
 import re
 import colorama
+import backoff
 from colorama import Fore
 from time import sleep
 
@@ -21,18 +22,17 @@ def logger(success: bool, uid, name, status: int):
 def clean_attrib(html_str: str):
     cleaner = Cleaner(safe_attrs_only=True, safe_attrs=frozenset({"class", "id"})) # Add the remove_empty_space parameter
     return cleaner.clean_html(minify(html_str.replace("<td>:</td>", ""), keep_closing_tags=True))
+@backoff.on_exception (backoff.expo, requests.exceptions.Timeout, max_time=60)
+def make_request(url: str, params):
+    response = requests.get(url, timeout=60, params=params, headers=headers)
+    return response
 
 colorama.init()
 headers = {"User-Agent": f"python-requests/{requests.__version__} (PointCollector/1.2; +https://fallback.choomai.xyz/bots/collector.txt)"}
 ts10_url = "http://khaothi.thuathienhue.edu.vn:8080/tracuu/chitietthcs.html"
 
 for id in range(config["ts10_start_ID"], config["ts10_end_ID"]):
-    req = requests.get(ts10_url, params={"id": id}, headers=headers)
-    if req.status_code == 429:
-        print("Got an 429, waiting for 1m server cooldown.")
-        sleep(60)
-        id -= 1
-        continue
+    req = make_request(ts10_url, params={"id": id}, timeout=60)
     html = BeautifulSoup(clean_attrib(req.text), "lxml")
 
     table = html.find(lambda tag: tag.name == "table" and "KẾT QUẢ THI" in tag.text).tr.td.div.table

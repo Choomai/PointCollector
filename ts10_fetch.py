33154a5701035c3aa0981492e60e9c0ca12073c9
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-06-20 17:08:13 +0700

    More info when logging

--- ts10_fetch.py
+++ ts10_fetch.py
@@ -15,16 +15,16 @@ for index in config:
     if not config[index] in ["true", "false"]: config[index] = int(config[index])
     elif config[index] == "true": config[index] = True
     elif config[index] == "false": config[index] = False
-def logger(success: bool, uid, name):
-    if success: print(f"{Fore.LIGHTGREEN_EX}Success | {uid} | {name}.{Fore.WHITE}")
-    else: print(f"{Fore.LIGHTRED_EX}Failed | {uid} | {name}.{Fore.WHITE}")
+def logger(success: bool, uid, name, status: int):
+    if success: print(f"{Fore.LIGHTGREEN_EX}Success | {status} | {uid} | {name}.{Fore.WHITE}")
+    else: print(f"{Fore.LIGHTRED_EX}Failed | {status} | {uid} | {name}.{Fore.WHITE}")
+def clean_attrib(html_str: str):
+    cleaner = Cleaner(safe_attrs_only=True, safe_attrs=frozenset({"class", "id"})) # Add the remove_empty_space parameter
+    return cleaner.clean_html(minify(html_str.replace("<td>:</td>", ""), keep_closing_tags=True))
 
 colorama.init()
 headers = {"User-Agent": f"python-requests/{requests.__version__} (compatible; PointCollector/1.2; +https://fallback.choomai.xyz/bots/collector.txt)"}
 ts10_url = "http://khaothi.thuathienhue.edu.vn:8080/tracuu/chitietthcs.html"
-def clean_attrib(html_str: str):
-    cleaner = Cleaner(safe_attrs_only=True, safe_attrs=frozenset({"class", "id"})) # Add the remove_empty_space parameter
-    return cleaner.clean_html(minify(html_str.replace("<td>:</td>", ""), keep_closing_tags=True))
 
 for id in range(config["ts10_start_ID"], config["ts10_end_ID"]):
     req = requests.get(ts10_url, params={"id": id}, headers=headers)
@@ -49,5 +49,5 @@ for id in range(config["ts10_start_ID"], config["ts10_end_ID"]):
         with open(f"./collected/ts10/{id}_{name}.json", "w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4) # Write the dictionary object to the JSON file
             file.close()
-            logger(True, id, name)
-    else: logger(False, id, name)
+            logger(True, id, name, req.status_code)
+    else: logger(False, id, name, req.status_code)

9d9a3e236aa7c2098e48b585fa79783ab7b95bc4
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-07-16 11:49:50 +0700

    Something has changed.

--- ts10_fetch.py
+++ ts10_fetch.py
@@ -1,30 +1,15 @@
 import requests
 from dotenv import dotenv_values
 from bs4 import BeautifulSoup
-import lxml
 from lxml.html.clean import Cleaner
 from minify_html import minify
 import json
-import re
 import colorama
 import backoff
-from colorama import Fore
-from time import sleep
+from extension import logger
 
 config = dotenv_values(".env")
-for index in config:
-    try: 
-        config[index] = int(config[index])
-        continue
-    except ValueError: pass
 
-    try: 
-        config[index] = bool(config[index])
-        continue
-    except ValueError: pass
-def logger(success: bool, uid, name, status: int):
-    if success: print(f"{Fore.LIGHTGREEN_EX}Success | {status} | {uid} | {name}.{Fore.WHITE}")
-    else: print(f"{Fore.LIGHTRED_EX}Failed | {status} | {uid} | {name}.{Fore.WHITE}")
 def clean_attrib(html_str: str):
     cleaner = Cleaner(safe_attrs_only=True, safe_attrs=frozenset({"class", "id"})) # Add the remove_empty_space parameter
     return cleaner.clean_html(minify(html_str.replace("<td>:</td>", ""), keep_closing_tags=True))
@@ -34,11 +19,11 @@ def make_request(url: str, params):
     return response
 
 colorama.init()
-headers = {"User-Agent": f"python-requests/{requests.__version__} (PointCollector/{config.app_ver}; +https://fallback.choomai.lol/bots/collector.txt)"}
-ts10_url = "http://khaothi.thuathienhue.edu.vn:8080/tracuu/chitietthcs.html"
+headers = {"User-Agent": f"python-requests/{requests.__version__} (PointCollector/{config["app_ver"]}; +https://fallback.choomai.lol/bots/collector.txt)"}
+ts10_url = "http://117.3.133.1:8080/tracuu/chitietthcs.html"
 
-for id in range(config["ts10_start_ID"], config["ts10_end_ID"]):
-    req = make_request(ts10_url, params={"id": id})
+for id in range(*map(int, (config["ts10_start_ID"], config["ts10_end_ID"]))):
+    req = make_request(ts10_url, params={"id": id, "kt": 1})
     html = BeautifulSoup(clean_attrib(req.text), "lxml")
 
     table = html.find(lambda tag: tag.name == "table" and "KẾT QUẢ THI" in tag.text).tr.td.div.table
